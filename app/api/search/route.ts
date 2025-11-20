1697286c1da11eb1bbe239fcc9945e10ef5d076a
Jaydrey <jaribwetshi64@gmail.com>
2025-11-20 12:44:19 +0300

    fix: updated the endpoints to use the new url

--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -3,7 +3,7 @@ export const dynamic = "force-dynamic"; // no cache
 
 const UPSTREAM =
   process.env.UPSTREAM_SEARCH ??
-  "https://7a4d4f14fd68.ngrok-free.app/webhook/a4cfdee8-25f9-4c3f-bda6-c2571f1975c5";
+  "https://impavidly-arguable-cicely.ngrok-free.dev/webhook/a4cfdee8-25f9-4c3f-bda6-c2571f1975c5";
 
 const CORS: Record<string, string> = {
   "Access-Control-Allow-Origin": "*",
